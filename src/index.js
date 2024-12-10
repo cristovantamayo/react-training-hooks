b65e208f36a67fb34f7209e4c2c6e740de090cd3
Cristovan Tamayo <ctamayo@bionexo.com>
2024-12-10 17:33:24 -0300

    useReduce inside UseContext aproach

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./AppUseReduce";
+import App from "./AppUseContextWithReduce";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
