251c89b7f10c1c0b4b2b32a75bc1b0b530f5c41a
Cristovan Tamayo <ctamayo@bionexo.com>
2024-12-10 19:26:11 -0300

    My Own Hook created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./AppUseContextWithReduce";
+import App from "./AppUseMyOwnHook";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
