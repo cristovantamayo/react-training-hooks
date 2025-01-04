5185a1e4ec88f510e3f03202bb3018cb92fa3d96
Cristovan Tamayo <ctamayo@bionexo.com>
2025-01-04 15:28:45 -0300

    Route-and-useParam

--- src/templates/Abc/index.jsx
+++ src/templates/Abc/index.jsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
 export const Abc = () => {
+  const { slug, id } = useParams();
+
   return (
     <div className="Abc">
-      <h1>Abc</h1>
+      <h1>
+        Abc {slug} {id}
+      </h1>
     </div>
   );
 };
