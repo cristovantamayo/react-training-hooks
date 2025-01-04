ffc06ab545fd8829566805313bba266421c6fe23
Cristovan Tamayo <ctamayo@bionexo.com>
2025-01-04 16:09:56 -0300

    Anchors causes Full Reload

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -2,10 +2,20 @@ import { Link } from "react-router-dom";
 
 export const Menu = () => {
   return (
-    <nav className="Menu">
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        padding: "1rem",
+        background: "black",
+        color: "white",
+      }}
+      className="Menu"
+    >
       <Link to="/">Home</Link>
-      <br />
+      <a href="/">Home (A)</a>
       <Link to="/abc">Abc</Link>
+      <a href="/abc">Abc (A)</a>
     </nav>
   );
 };
