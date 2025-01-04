fca7397a98a6a1ca7a4feff33c61b838844e2b62
Cristovan Tamayo <ctamayo@bionexo.com>
2025-01-04 15:59:56 -0300

    Page NotFound added

--- src/templates/Abc/index.jsx
+++ src/templates/Abc/index.jsx
@@ -2,15 +2,6 @@ import { useEffect } from "react";
 import { useParams, useLocation, useHistory } from "react-router-dom";
 export const Abc = () => {
   const { slug, id } = useParams();
-  const location = useLocation();
-  const history = useHistory();
-
-  //console.log(history);
-  useEffect(() => {
-    setTimeout(() => {
-      history.push("/");
-    }, 5000);
-  }, [history]);
 
   return (
     <div className="Abc">
---
+++ src/templates/Page404/index.jsx
@@ -0,0 +1,7 @@
+export const Page404 = () => {
+  return (
+    <div className="Page404">
+      <h1>Error 404 - Page not found!</h1>
+    </div>
+  );
+};
